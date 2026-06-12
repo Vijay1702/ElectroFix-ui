2f1047f498d2cf0bc78c785516cba249dace0fd8
Vijay Ramesh <vijay.ram@finforz.com>
2026-06-12 10:23:38 +0530

    chore: add sonarjs and security plugins to eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import sonarjs from 'eslint-plugin-sonarjs'
+import security from 'eslint-plugin-security'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,6 +16,8 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      sonarjs.configs.recommended,
+      security.configs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
